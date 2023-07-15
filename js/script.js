06cb0e318e3cb5be91f6efad0180a8d086a843ec
paydovan <jpmpadovan@gmail.com>
2023-07-15 15:18:10 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: false });
+
+sr.reveal('.name-text', { duration: 1000});
+
+sr.reveal('.carousel-images', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration:2000
+});
+
+sr.reveal('.text-contact-me', {
+    rotate: { x: 80, y: 0, z: 0},
+    duration:2000,
+});
